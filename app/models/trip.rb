24e70715aaf8c409e0051a63acf7252052efb579
gabysurianosj <surianogabriela@gmail.com>
2018-11-09 17:48:19 +0100

    added country and categories to models

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,4 +2,7 @@ class Trip < ApplicationRecord
   has_many :mood_trips
   has_many :moods, through: :mood_trips
   has_many :users, through: :mood_trips
+  has_many :trip_categories
+  has_many :categories, through: :trip_categories
+  belongs_to :country
 end
