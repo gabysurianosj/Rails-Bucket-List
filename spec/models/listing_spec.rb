04b98bdf11a473e4bf6b0b9962e342ffbe840cad
gabysurianosj <surianogabriela@gmail.com>
2018-11-11 12:02:14 +0100

    added listing spec

---
+++ spec/models/listing_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Listing, type: :model do
+  before(:all) do
+    @listing1 = create(:listing)
+  end
+
+  it 'is valid with valid attributes' do
+    expect(@listing1).to be_valid
+  end
+
+  it { is_expected.to belong_to :todolist }
+
+  it { is_expected.to belong_to :trip }
+
+  it { is_expected.to belong_to :user }
+
+  it 'is not valid if the season is not a string' do
+    listing2 = build(:listing, user_season: "22")
+    expect(listing2).to_not be_valid
+  end
+
+end
--- spec/models/mood_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
- RSpec.describe Mood, type: :model do
-  it "can be created" do
-    user = User.create(email: 'test@123.com', password: 'test123')
-    mood = Mood.create(name: 'My Mood', user: user)
-    expect(mood).to be_valid
-  end
-end
